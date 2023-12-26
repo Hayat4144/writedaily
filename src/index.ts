6563c2e9dc05a7159d2d6a66ff751fb64b0e90c1
Hayat ilyas <ihayat855@gmail.com>
2023-12-26 15:44:18 +0530

    adding vercel.json and cluster module

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,29 @@ import helmet from 'helmet';
 import hpp from 'hpp';
 import logger from '@utils/logger';
 import { httpStatusCode } from './types';
+import { cpus } from 'os';
+import cluster from 'cluster';
 
-const app = express();
-const port = process.env.PORT || 8080;
+const numCPUs = cpus().length;
 
-// app configuration
+const StartServer = () => {
+    const app = express();
+    const port = process.env.PORT || 8080;
 
-const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 100,
-    message: 'You have exceeded the 100 requests in 10 minute limit!',
-    standardHeaders: true,
-    legacyHeaders: false,
-});
+    // app configuration
+    const limiter = rateLimit({
+        windowMs: 10 * 60 * 1000,
+        max: 100,
+        message: 'You have exceeded the 100 requests in 10 minute limit!',
+        standardHeaders: true,
+        legacyHeaders: false,
+    });
 
-app.use(helmet());
-app.use(limiter);
-/*
-app.use(
+    app.use(helmet());
+    app.use(limiter);
+
+    /*
+    app.use(
     cors({
         origin:
             process.env.NODE_ENV === 'production'
@@ -43,24 +48,47 @@ app.use(
 );
 
 */
-app.use(cors());
-app.use(express.json());
-app.use(hpp());
-app.disable('x-powered-by');
+    app.use(cors());
+    app.use(express.json());
+    app.use(hpp());
+    app.disable('x-powered-by');
 
-//app routes
-app.get('/', (req, res) => {
-    return res.status(httpStatusCode.OK).json({ data: 'Hello developers' });
-});
-app.use(authrouter);
-app.use(blogsroutes);
-app.use(commentroutes);
-app.use(likeroutes);
-app.use(followroutes);
-app.use(topicRoutes);
-app.use(readinglistRoutes);
-app.use(ErrorMiddleware);
+    //app routes
+    app.get('/', (req, res) => {
+        return res.status(httpStatusCode.OK).json({ data: 'Hello developers' });
+    });
+    app.use(authrouter);
+    app.use(blogsroutes);
+    app.use(commentroutes);
+    app.use(likeroutes);
+    app.use(followroutes);
+    app.use(topicRoutes);
+    app.use(readinglistRoutes);
+    app.use(ErrorMiddleware);
 
-app.listen(port, async () => {
-    logger.info(`Sever is running at port ${port}`);
-});
+    app.listen(port, async () => {
+        logger.info(`Sever is running at port ${port}`);
+    });
+};
+
+process.env.NODE_ENV === 'production'
+    ? cluster.isPrimary
+        ? (() => {
+              logger.info(`The primary process id is ${process.pid}`);
+
+              for (let index = 0; index < numCPUs; index++) {
+                  const worker = cluster.fork();
+
+                  worker.on('exit', (code, signal) => {
+                      if (signal) {
+                          logger.info(`worker was killed by signal: ${signal}`);
+                      } else if (code !== 0) {
+                          logger.info(`worker exited with error code: ${code}`);
+                      } else {
+                          console.info('worker success!');
+                      }
+                  });
+              }
+          })()
+        : StartServer()
+    : StartServer();
