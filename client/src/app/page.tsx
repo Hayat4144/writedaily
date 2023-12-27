f9388c9b8e44d8f992cd9fd6d26688370554b582
Hayat ilyas <ihayat855@gmail.com>
2023-12-27 18:06:17 +0530

    Initailize the app

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+    return (
+        <main className="my-5 mx-5">
+            <h1 className="text-2xl">Your plate Editor is Here</h1>
+        </main>
+    );
+}
